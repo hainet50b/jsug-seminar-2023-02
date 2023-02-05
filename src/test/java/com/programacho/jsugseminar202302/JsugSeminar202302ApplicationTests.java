5ebcc5528a5dbd5adfca40f1906bd5a2102e5e78
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-02-05 20:20:18 +0900

    init

---
+++ src/test/java/com/programacho/jsugseminar202302/JsugSeminar202302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacho.jsugseminar202302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsugSeminar202302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
