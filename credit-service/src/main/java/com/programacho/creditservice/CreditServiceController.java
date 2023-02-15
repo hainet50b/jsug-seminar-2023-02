1dddbdb99d49d7c7cb14117a08e6ad9db4c6887d
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-02-15 19:01:46 +0900

    credit-serviceの業務ログを追加した。

--- credit-service/src/main/java/com/programacho/creditservice/CreditServiceController.java
+++ credit-service/src/main/java/com/programacho/creditservice/CreditServiceController.java
@@ -1,5 +1,7 @@
 package com.programacho.creditservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CreditServiceController {
 
+    private final Logger log = LoggerFactory.getLogger(CreditServiceController.class);
+
     @PostMapping("/authorize")
     public CreditAuthorizeResponse authorize(@RequestBody CreditAuthorizeRequest request) {
+        log.info("クレジットカードの与信を取得しました。");
+
         return new CreditAuthorizeResponse("ok", null);
     }
 }
