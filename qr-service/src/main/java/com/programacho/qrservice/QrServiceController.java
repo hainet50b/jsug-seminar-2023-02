76cb1f1a8715e2f96b673753e5c1f48668567bc8
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-02-15 19:04:16 +0900

    qr-serviceの業務をログを追加した。

--- qr-service/src/main/java/com/programacho/qrservice/QrServiceController.java
+++ qr-service/src/main/java/com/programacho/qrservice/QrServiceController.java
@@ -1,5 +1,7 @@
 package com.programacho.qrservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QrServiceController {
 
+    private final Logger log = LoggerFactory.getLogger(QrServiceController.class);
+
     @PostMapping("/create-code")
     public QrCreateCodeResponse createCode(@RequestBody QrCreateCodeRequest request) {
+        log.info("QRコードを発行しました。");
+
         return new QrCreateCodeResponse("ok", null);
     }
 }
