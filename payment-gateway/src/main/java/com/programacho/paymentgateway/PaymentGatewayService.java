3981d2b6173d1d9f1a1e5b1bfd2e9808aed32f44
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-02-16 15:21:37 +0900

    特定ノードの場合に決済トランザクションの確定処理が遅延する処理を実装した。

--- payment-gateway/src/main/java/com/programacho/paymentgateway/PaymentGatewayService.java
+++ payment-gateway/src/main/java/com/programacho/paymentgateway/PaymentGatewayService.java
@@ -1,17 +1,29 @@
 package com.programacho.paymentgateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class PaymentGatewayService {
 
+    private final Logger log = LoggerFactory.getLogger(PaymentGatewayService.class);
+
     public void commitTransaction() {
-        try {
-            TimeUnit.MILLISECONDS.sleep(100);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        if (MDC.get("service.node.name").equals("node-2")) {
+            Random random = new Random();
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(random.nextInt(3_000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+        log.info("決済トランザクションの確定が完了しました。");
     }
 }
