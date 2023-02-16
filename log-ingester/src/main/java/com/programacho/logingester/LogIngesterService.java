d58323949a7b51a4adadf3d6e640759390cad5fc
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-02-16 18:22:22 +0900

    分散トレーシングを導入した。

--- log-ingester/src/main/java/com/programacho/logingester/LogIngesterService.java
+++ log-ingester/src/main/java/com/programacho/logingester/LogIngesterService.java
@@ -1,5 +1,7 @@
 package com.programacho.logingester;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -8,17 +10,22 @@ public class LogIngesterService {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public LogIngesterService(JdbcTemplate jdbcTemplate) {
+    private final ObservationRegistry observationRegistry;
+
+    public LogIngesterService(JdbcTemplate jdbcTemplate, ObservationRegistry observationRegistry) {
         this.jdbcTemplate = jdbcTemplate;
+        this.observationRegistry = observationRegistry;
     }
 
     public void insert(LogIngesterPayload payload) {
-        jdbcTemplate.update(
-                "INSERT INTO exchange_log (application, type, direction, body) VALUES (?, ?, ?, ?)",
-                payload.application(),
-                payload.type(),
-                payload.direction(),
-                payload.body()
-        );
+        Observation
+                .createNotStarted("insert into exchange_log", observationRegistry)
+                .observe(() -> jdbcTemplate.update(
+                        "INSERT INTO exchange_log (application, type, direction, body) VALUES (?, ?, ?, ?)",
+                        payload.application(),
+                        payload.type(),
+                        payload.direction(),
+                        payload.body()
+                ));
     }
 }
