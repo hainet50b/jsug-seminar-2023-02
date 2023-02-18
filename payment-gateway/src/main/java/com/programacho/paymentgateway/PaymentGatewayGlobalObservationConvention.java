c5b3172b6bb82d527ae87d0459f58a15eafb9d1b
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-02-18 19:08:54 +0900

    MDCをトレースのタグに含める実装を改善した。

--- payment-gateway/src/main/java/com/programacho/paymentgateway/PaymentGatewayGlobalObservationConvention.java
+++ payment-gateway/src/main/java/com/programacho/paymentgateway/PaymentGatewayGlobalObservationConvention.java
@@ -4,46 +4,64 @@ import io.micrometer.common.KeyValue;
 import io.micrometer.common.KeyValues;
 import io.micrometer.observation.GlobalObservationConvention;
 import io.micrometer.observation.Observation;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PaymentGatewayGlobalObservationConvention implements GlobalObservationConvention<Observation.Context> {
 
     @Override
-    public boolean supportsContext(Observation.Context context) {
+    public boolean supportsContext(@NotNull Observation.Context context) {
         return true;
     }
 
     @Override
+    @NotNull
     public KeyValues getLowCardinalityKeyValues(Observation.Context context) {
-        // General
-        if (MDC.get("service.node.name") != null) context.addLowCardinalityKeyValue(KeyValue.of("service.node.name", MDC.get("service.node.name")));
-        if (MDC.get("labels.function") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.function", MDC.get("labels.function")));
-        if (MDC.get("labels.endpoint") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.endpoint", MDC.get("labels.endpoint")));
+        List<String> keys = List.of(
+                // General
+                "service.node.name",
+                "labels.function",
+                "labels.endpoint",
+                // payment-gateway
+                "labels.payment-gateway.result",
+                "labels.payment-gateway.error-code",
+                // payment
+                "labels.payment.result",
+                "labels.payment.error-code"
+        );
 
-        // payment-gateway
-        if (MDC.get("labels.payment-gateway.result") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment-gateway.result", MDC.get("service.node.name")));
-        if (MDC.get("labels.payment-gateway.error-code") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment-gateway.error-code", MDC.get("labels.payment-gateway.error-code")));
-
-        // payment
-        if (MDC.get("labels.payment.result") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment.result", MDC.get("labels.payment.result")));
-        if (MDC.get("labels.payment.error-code") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment.error-code", MDC.get("labels.payment.error-code")));
+        MDC.getCopyOfContextMap().forEach((k, v) -> {
+            if (keys.contains(k)) {
+                context.addLowCardinalityKeyValue(KeyValue.of(k, v));
+            }
+        });
 
         return context.getLowCardinalityKeyValues();
     }
 
     @Override
+    @NotNull
     public KeyValues getHighCardinalityKeyValues(Observation.Context context) {
-        if (MDC.get("labels.user") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.user", MDC.get("labels.user")));
+        List<String> keys = List.of(
+                // General
+                "labels.user",
+                // payment-gateway
+                "labels.payment-gateway.id",
+                // payment
+                "labels.payment.id",
+                "labels.payment.token",
+                "labels.payment.qr.url"
+        );
 
-        // payment-gateway
-        if (MDC.get("labels.payment-gateway.id") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment-gateway.id", MDC.get("labels.payment-gateway.id")));
-
-        // payment
-        if (MDC.get("labels.payment.id") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment.id", MDC.get("labels.payment.id")));
-        if (MDC.get("labels.payment.token") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment.token", MDC.get("labels.payment.token")));
-        if (MDC.get("labels.payment.qr.url") != null) context.addLowCardinalityKeyValue(KeyValue.of("labels.payment.qr.url", MDC.get("labels.payment.qr.url")));
+        MDC.getCopyOfContextMap().forEach((k, v) -> {
+            if (keys.contains(k)) {
+                context.addHighCardinalityKeyValue(KeyValue.of(k, v));
+            }
+        });
 
         return context.getHighCardinalityKeyValues();
     }
